167cf3f7979be94841d635167bdf5c7405ba9ddb
nladuo <kalen25115@gmail.com>
2017-04-18 14:21:41 +0800

    add code of lesson11.

--- lesson11/kmeans_clustering.py
+++ lesson11/kmeans_clustering.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     with open("../lesson10/dataset.pickle", "rb") as f:
         X = np.load(f)
-        print X.shape
+        print "shape of dataset:", X.shape
 
     with open("../lesson10/segments.pickle", "rb") as f:
         segments = pickle.load(f)
@@ -19,12 +19,4 @@ if __name__ == "__main__":
     print "done in %0.3fs" % (time() - t0)
 
     with open("km.pickle", "wb") as f:
-        pickle.dump(km, f)
-
-
-
-
-
-
-
-
+        pickle.dump(km, f, pickle.HIGHEST_PROTOCOL)
