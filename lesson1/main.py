588f1c08654bc57a39f76cd76acb0e510187cb8f
nladuo <kalen25115@gmail.com>
2017-09-03 22:10:19 +0800

    updated code.

--- lesson1/main.py
+++ lesson1/main.py
@@ -6,9 +6,13 @@ from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     resp = requests.get("http://scce.ustb.edu.cn/more.action?categoryId=1")
+    # print resp.content
     soup = BeautifulSoup(resp.content)
-    items = soup.find_all("div", {"class", "list_title"})
+    items = soup.find_all("div", {"class": "every_list"})
 
-    for index, item in enumerate(items):
-        print index, item.get_text().replace("\n", "")
-        print item.a['href']
+    for item in items:
+        title_div = item.find("div", {"class": "list_title"})
+        title = title_div.a.get_text()
+        url = title_div.a["href"]
+        time = item.find("div", {"class": "list_time"}).get_text()
+        print time, title, url
--- lesson2/main.py
+++ lesson2/main.py
@@ -3,7 +3,7 @@
 import requests
 session = requests.session()
 
-session.post("http://tx.kalen.site/crawler_lesson2/do_login.php",
+session.post("http://123.206.86.230/crawler_lesson2/do_login.php",
              data={"uname": "nladuo", "passwd": "nladuo"})  # 登陆
-resp = session.get("http://tx.kalen.site/crawler_lesson2/private.php")
+resp = session.get("http://123.206.86.230/crawler_lesson2/private.php")
 print resp.content
