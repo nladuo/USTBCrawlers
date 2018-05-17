97ea202de85bdfcb98fc38b1d4dbe5a78110c883
nladuo <kalen25115@gmail.com>
2018-05-17 10:47:17 +0800

    add lesson5-demo.

---
+++ lesson5/new-simple-framework/SimpleCrawler.py
@@ -0,0 +1,30 @@
+# coding=utf8
+""" 简易爬虫框架 """
+from multiprocessing import Manager, Pool
+
+
+class SimpleCrawler:
+    def __init__(self, c_num):
+        self.task_queue = Manager().Queue()  # 任务队列
+
+        self.workers = {}                    # Worker, 字典类型, 存放不同的Worker
+        self.c_num = c_num                   # 并发数,开几个进程
+
+    def add_task(self, task):
+        self.task_queue.put(task)
+
+    def add_worker(self, identifier, worker):
+        self.workers[identifier] = worker
+
+    def start(self):
+        pool = Pool(self.c_num)
+        lock = Manager().Lock()
+        while True:
+            task = self.task_queue.get(True)
+            if task['id'] == "NO":  # 结束爬虫
+                pool.close()
+                pool.join()
+                exit(0)
+            else:  # 给worker完成任务
+                worker = self.workers[task['id']]
+                pool.apply_async(worker, args=(self.task_queue, task, lock))
---
+++ lesson5/new-simple-framework/main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding=utf8
+""" 并行爬取北科计通学院新闻及其详情 """
+from SimpleCrawler import *
+import requests
+from bs4 import BeautifulSoup
+import pymongo
+
+
+def worker(queue, task, lock):
+    """ 爬取新闻列表页 """
+    # 下载任务
+    url = task["url"] + "%d.html" % task["page"]
+    print "downloading:", url
+    resp = requests.get(url)
+    # 解析网页
+    soup = BeautifulSoup(resp.content, "html.parser")
+    items = soup.find_all("div", {"class", "list_title"})
+
+    for index, item in enumerate(items):
+        detail_url = "http://nladuo.cn/scce_site/" + item.a['href']
+        print "adding:", detail_url
+        # 添加新任务: 爬取详情页
+        queue.put({
+            "id": "detail_worker",
+            "url": detail_url,
+            "page": task["page"],
+            "index": index,
+            "title": item.get_text().replace("\n", "")
+        })
+
+    if task["page"] == 10:  # 添加结束信号
+        queue.put({"id": "NO"})
+    else:
+        # 添加新任务: 爬取下一页
+        queue.put({
+            "id": "worker",
+            "url": "http://nladuo.cn/scce_site/",
+            "page": task["page"]+1
+        })
+
+
+def detail_worker(queue, task, lock):
+    """ 爬取新闻详情页 """
+    # 下载任务
+    print "downloading:", task['url']
+    resp = requests.get(task['url'])
+    # 解析网页
+    soup = BeautifulSoup(resp.content, "html.parser")
+    click_num = soup.find("div", {"class", "artNum"}).get_text()
+    print task["page"], task["index"], task['title'], click_num
+    with lock:
+        insert_news({
+            "title": task["title"],
+            "click_num": click_num
+        })
+
+
+def init_collection():
+    client = pymongo.MongoClient(host="localhost", port=27017, connect=False)
+    db = client['test']
+    return db["news"]
+
+
+def insert_news(data):
+    collection.insert(data)
+
+
+if __name__ == '__main__':
+    collection = init_collection()
+    crawler = SimpleCrawler(5)
+    crawler.add_worker("worker", worker)
+    crawler.add_worker("detail_worker", detail_worker)
+    crawler.add_task({
+        "id": "worker",
+        "url": "http://nladuo.cn/scce_site/",
+        "page": 1
+    })
+    crawler.start()
+
