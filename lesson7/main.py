a9e54a5873f15161281fe1557c75148bcc2324ca
nladuo <kalen25115@gmail.com>
2018-12-08 21:08:34 +0800

    updated lesson7-code.

--- lesson7/main.py
+++ lesson7/main.py
@@ -6,10 +6,10 @@ def parse_ip(content):
     soup = BeautifulSoup(content, "html.parser")
     print(soup.body.get_text())
 
-resp = requests.get("http://1212.ip138.com/ic.asp")
+resp = requests.get("http://2018.ip138.com/ic.asp")
 parse_ip(resp.content.decode("gbk"))
 
-print("改变ip后:")
-proxies = {"http": "http://175.155.25.77:808"}
-resp2 = requests.get("http://1212.ip138.com/ic.asp", proxies=proxies)
+print("\n改变ip后:")
+proxies = {"http": "http://139.129.207.72:808"}   # 需要改成在西刺代理上看到的可用代理ip
+resp2 = requests.get("http://2018.ip138.com/ic.asp", proxies=proxies)
 parse_ip(resp2.content.decode("gbk"))
