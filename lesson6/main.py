fcc9680172bb5d6cc750cd9ce9a1eb2a703ceaa2
nladuo <kalen25115@gmail.com>
2018-12-08 17:10:52 +0800

    updated code of lesson6.

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,16 +1,15 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from selenium import webdriver
-import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-exe_path = "/Users/kalen/Programfiles/phantomjs-2.1.1-macosx/bin/phantomjs"
-driver = webdriver.PhantomJS(executable_path=exe_path)
+dcap = dict(DesiredCapabilities.PHANTOMJS)
+dcap['phantomjs.page.settings.userAgent'] = ('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36')
+exe_path = "/usr/bin/phantomjs"
+driver = webdriver.PhantomJS(executable_path=exe_path, desired_capabilities=dcap)
 
-driver.get("http://baidu.com/")
+driver.get("https://www.baidu.com/s?wd=%E7%BE%8E%E5%A5%B3")
 
-time.sleep(1)
-print(driver.page_source)
+driver.save_screenshot("screenshot.png")  # 截个图
+print(driver.page_source)  # 打印源码
 
 driver.quit()
 
