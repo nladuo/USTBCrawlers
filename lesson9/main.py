dd2bb68690b5598b73da3883c73e4ee6be4624c8
nladuo <kalen25115@gmail.com>
2018-12-13 01:25:08 +0800

    modified:   lesson9/main.py

--- lesson9/main.py
+++ lesson9/main.py
@@ -18,31 +18,33 @@ def save_docs(docs):
 def worker(queue, task, lock):
     offset = (task["page"] - 1) * 50
     print("downloading: page %d" % task["page"])
+    # 1. 下载页面
     resp = requests.get("http://tieba.baidu.com/f?kw="
                         "%E5%8C%97%E4%BA%AC%E7%A7%91%E6%8A%80%E5%A4%A7%E5%AD%A6&ie=utf-8&pn=" + str(offset))
     soup = BeautifulSoup(resp.content, "html.parser")
+
+    # 2. 解析页面
     items = soup.find_all("a", {"class", "j_th_tit"})
 
     docs = []
     for index, item in enumerate(items):
         docs.append({"page": task["page"], "index": index, "title": item.get_text()})
         print(task["page"], index, item.get_text())
+    # 3. 保存数据
     with lock:
         save_docs(docs)
 
-    if task["page"] > 1000:
+    # 4. 添加新任务
+    if (task["page"] + 10) > 1000:
         queue.put({"id": "NO"})
-    elif task["page"] % 10 == 0:
-        start_page = task["page"] + 1
-        end_page = task["page"] + 11
-        for i in range(start_page, end_page):
-            queue.put({"id": "worker", "page": i})
+    else:
+        queue.put({"id": "worker", "page": task["page"] + 10})
 
+beike = init_collection()
 
 if __name__ == '__main__':
-    beike = init_collection()
     crawler = SimpleCrawler(5)
+    crawler.add_worker("worker", worker)
     for i in range(1, 11):
         crawler.add_task({"id": "worker", "page": i})
-    crawler.add_worker("worker", worker)
     crawler.start()
